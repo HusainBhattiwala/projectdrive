def42e6b5dbf4bb0e7f0db09bbb19de490d6adfc
Husain007 <husainbhattiwala5253@gmail.com>
2024-06-10 17:52:54 +0530

    fleet page ui update

--- rolnew/comp/CarClass.jsx
+++ rolnew/comp/CarClass.jsx
@@ -1,35 +1,35 @@
-import Pic from 'rolnew/util/Pic';
-import Button from 'rolnew/ui/Button';
-import Category from './Category';
+import Pic from "rolnew/util/Pic";
+import Button from "rolnew/ui/Button";
+import Category from "./Category";
 
 const carCategoryList = [
   {
-    id: 'carcat1',
-    category: 'Business Class',
+    id: "carcat1",
+    category: "Business Class",
   },
   {
-    id: 'carcat2',
-    category: 'First Class',
+    id: "carcat2",
+    category: "First Class",
   },
   {
-    id: 'carcat3',
-    category: 'Luxury Class',
+    id: "carcat3",
+    category: "Luxury Class",
   },
   {
-    id: 'carcat4',
-    category: 'Electric',
+    id: "carcat4",
+    category: "Electric",
   },
   {
-    id: 'carcat5',
-    category: 'SUV',
+    id: "carcat5",
+    category: "SUV",
   },
   {
-    id: 'carcat6',
-    category: 'MVP',
+    id: "carcat6",
+    category: "MVP",
   },
   {
-    id: 'carcat7',
-    category: 'Sprinter',
+    id: "carcat7",
+    category: "Sprinter",
   },
 ];
 
@@ -49,78 +49,130 @@ function CarClass() {
 
       </Container> */}
 
-      <div className="bg-[#11202D] pt-4 sm:pt-[10px] text-center xs:pt-16 xl:!px-[70px] lg:px-[45px] md:px-[32px] sm:px-[20px] px-4">
-        <Category categorys={carCategoryList} />
-        <div className="bg-[#11202D] xs:pt-12 sm:px-12 md:px-16 pb-6">
-          <div className="flex flex-col gap-y-16 2xl:container mx-auto pb-4">
-            <div className="flex md:flex-row xs:flex-col sm:flex-col">
-              <div className="w-full hidden sm:block">
-
-                <div className=" gap-[24px] w-[360px] h-[298px]">
-                  <div className="">
-                    <div className="sm:mt-6 mt-2 text-[#B2B2B2] leading-5">
-                      <h2 className="sm:text-3xl leading-9 text-left font-medium text-[#FFFFFF]">
+      <div className='bg-[#11202D] pt-4 sm:pt-[10px] text-center xs:pt-16 xl:!px-[70px] lg:px-[45px] md:px-[32px] sm:px-[20px] px-4'>
+        {/* <Category categorys={carCategoryList} /> */}
+        <div className='bg-[#11202D] xs:pt-12 sm:px-12 md:px-16 pb-6 mt-28'>
+          <div className='flex flex-col gap-y-16 2xl:container mx-auto pb-4'>
+            <div className='flex md:flex-row xs:flex-col sm:flex-col'>
+              <div className='w-full hidden sm:block'>
+                <div className=' gap-[24px] w-[360px] h-[298px]'>
+                  <div className=''>
+                    <div className='sm:mt-6 mt-2 text-[#B2B2B2] leading-5'>
+                      <h2 className='sm:text-3xl leading-9 text-left font-medium text-[#FFFFFF]'>
                         Mercedes Benz E class
                       </h2>
-                      <p className="sm:text-base font-medium sm:leading-6 mt-4 text-left text-[#FFF8F3]">
-                        The Mercedes E-Class is a great option for business because of its balance of luxury, performance, and technology. Mercedes Benz is known for their brilliant vehicles and truly stands by what they call its cars to be - &apos;The best or nothing&apos;. The car has a sleek, professional design and a spacious, comfortable interior that suits any businessman&apos;s needs. A Mercedes E Class hire is what most businessmen prefer for their travel needs.
+                      <p className='sm:text-base font-medium sm:leading-6 mt-4 text-left text-[#FFF8F3]'>
+                        The Mercedes E-Class is a great option for business
+                        because of its balance of luxury, performance, and
+                        technology. Mercedes Benz is known for their brilliant
+                        vehicles and truly stands by what they call its cars to
+                        be - &apos;The best or nothing&apos;. The car has a
+                        sleek, professional design and a spacious, comfortable
+                        interior that suits any businessman&apos;s needs. A
+                        Mercedes E Class hire is what most businessmen prefer
+                        for their travel needs.
                       </p>
-                      <Button className="w-[260px] h-[50px] text-xl mt-4 mb-4 mr-[100px]">Book Now</Button>
+                      <Button className='w-[260px] h-[50px] text-xl mt-4 mb-4 mr-[100px]'>
+                        Book Now
+                      </Button>
                     </div>
                   </div>
-
                 </div>
               </div>
-              <div className="w-full h-[370px] max-w-[705px] hidden md:block">
-                <div className="w-full h-full relative">
-                  <Pic alt="trust" className="rounded-xl w-[495px]" src="/images/airports/car1.png" objectFit="fit" />
+              <div className='w-full h-[370px] max-w-[705px] hidden md:block'>
+                <div className='w-full h-full relative'>
+                  <Pic
+                    alt='trust'
+                    className='rounded-xl w-[495px]'
+                    src='/images/airports/car1.png'
+                    objectFit='fit'
+                  />
                 </div>
               </div>
-              <div className="h-[210px] w-full mt-4 sm:mt-0  md:hidden ml-4">
-                <div className="w-[350px] sm:w-[450px] h-full">
-                  <Pic alt="trust" className="rounded-xl" src="/images/airports/car1.png" objectFit="fit" />
+              <div className='h-[210px] w-full mt-4 sm:mt-0  md:hidden ml-4'>
+                <div className='w-[350px] sm:w-[450px] h-full'>
+                  <Pic
+                    alt='trust'
+                    className='rounded-xl'
+                    src='/images/airports/car1.png'
+                    objectFit='fit'
+                  />
                 </div>
               </div>
-              <div className="w-full mx-auto sm:hidden">
-
-                <div className="w-full h-[298px]">
-                  <div className="min-w-[350px] w-[550px] mx-auto">
-                    <div className="mt-6 text-[#B2B2B2] leading-5 mx-2">
-                      <h2 className="text-[20px] leading-7 text-center font-medium text-[#FFFFFF] my-6 mt-2">
+              <div className='w-full mx-auto sm:hidden'>
+                <div className='w-full h-[298px]'>
+                  <div className='min-w-[350px] w-[550px] mx-auto'>
+                    <div className='mt-6 text-[#B2B2B2] leading-5 mx-2'>
+                      <h2 className='text-[20px] leading-7 text-center font-medium text-[#FFFFFF] my-6 mt-2'>
                         Mercedes Benz E class
                       </h2>
-                      <p className="text-sm font-normal leading-7 text-left text-[#FFF8F3] mt-4">
-                        The Mercedes E-Class is a great option for business because of its balance of luxury, performance, and technology. Mercedes Benz is known for their brilliant vehicles and truly stands by what they call its cars to be - &apos;The best or nothing&apos;. The car has a sleek, professional design and a spacious, comfortable interior that suits any businessman&apos;s needs. A Mercedes E Class hire is what most businessmen prefer for their travel needs.
+                      <p className='text-sm font-normal leading-7 text-left text-[#FFF8F3] mt-4'>
+                        The Mercedes E-Class is a great option for business
+                        because of its balance of luxury, performance, and
+                        technology. Mercedes Benz is known for their brilliant
+                        vehicles and truly stands by what they call its cars to
+                        be - &apos;The best or nothing&apos;. The car has a
+                        sleek, professional design and a spacious, comfortable
+                        interior that suits any businessman&apos;s needs. A
+                        Mercedes E Class hire is what most businessmen prefer
+                        for their travel needs.
                       </p>
-                      <Button className="w-full h-[50px] text-xl mt-4 mb-4 mr-[100px]">Book Now</Button>
+                      <Button className='w-full h-[50px] text-xl mt-4 mb-4 mr-[100px]'>
+                        Book Now
+                      </Button>
                     </div>
                   </div>
-
                 </div>
               </div>
             </div>
 
-            <div className="hidden max-w-[912px] h-[130px] sm:grid sm:grid-cols-4 lg:ml-[260px] md:ml-[130px] sm:ml-[30px] sm:mr-8 gap-16 mb-6">
-              <div className="w-[190px] h-[130px]">
-                <Pic alt="trust" className="rounded-xl" src="/images/airports/car4.png" objectFit="fit" />
-                <p className="w-full text-center text-base font-medium leading-6 text-[#FFFFFF]">Mercedes Benz E class</p>
+            <div className='hidden max-w-[912px] h-[130px] sm:grid sm:grid-cols-4 lg:ml-[260px] md:ml-[130px] sm:ml-[30px] sm:mr-8 gap-16 mb-6'>
+              <div className='w-[190px] h-[130px]'>
+                <Pic
+                  alt='trust'
+                  className='rounded-xl'
+                  src='/images/airports/car4.png'
+                  objectFit='fit'
+                />
+                <p className='w-full text-center text-base font-medium leading-6 text-[#FFFFFF]'>
+                  Mercedes Benz E class
+                </p>
               </div>
-              <div className="w-[190px] h-[130px]">
-                <Pic alt="trust" className="rounded-xl" src="/images/airports/car4.png" objectFit="fit" />
-                <p className="w-full text-center text-base font-medium leading-6 text-[#FFFFFF]">Mercedes Benz E class</p>
+              <div className='w-[190px] h-[130px]'>
+                <Pic
+                  alt='trust'
+                  className='rounded-xl'
+                  src='/images/airports/car4.png'
+                  objectFit='fit'
+                />
+                <p className='w-full text-center text-base font-medium leading-6 text-[#FFFFFF]'>
+                  Mercedes Benz E class
+                </p>
               </div>
-              <div className="w-[190px] h-[130px]">
-                <Pic alt="trust" className="rounded-xl" src="/images/airports/car4.png" objectFit="fit" />
-                <p className="w-full text-center text-base font-medium leading-6 text-[#FFFFFF]">Mercedes Benz E class</p>
+              <div className='w-[190px] h-[130px]'>
+                <Pic
+                  alt='trust'
+                  className='rounded-xl'
+                  src='/images/airports/car4.png'
+                  objectFit='fit'
+                />
+                <p className='w-full text-center text-base font-medium leading-6 text-[#FFFFFF]'>
+                  Mercedes Benz E class
+                </p>
               </div>
-              <div className="w-[190px] h-[130px]">
-                <Pic alt="trust" className="rounded-xl" src="/images/airports/car4.png" objectFit="fit" />
-                <p className="w-full text-center text-base font-medium leading-6 text-[#FFFFFF]">Mercedes Benz E class</p>
+              <div className='w-[190px] h-[130px]'>
+                <Pic
+                  alt='trust'
+                  className='rounded-xl'
+                  src='/images/airports/car4.png'
+                  objectFit='fit'
+                />
+                <p className='w-full text-center text-base font-medium leading-6 text-[#FFFFFF]'>
+                  Mercedes Benz E class
+                </p>
               </div>
             </div>
-
           </div>
-
         </div>
       </div>
     </>
--- rolnew/comp/FleetBanner.jsx
+++ rolnew/comp/FleetBanner.jsx
@@ -1,54 +1,146 @@
-function FleetBanner() {
+"use client";
+
+import Link from "next/link";
+import { useEffect, useState } from "react";
+
+function FleetBanner({ mainTitle }) {
+  const [width, setWidth] = useState();
+  const [height, setHeight] = useState(1000);
+  useEffect(() => {
+    function handleResize() {
+      setWidth(window.innerWidth);
+      setHeight(window.innerHeight);
+    }
+    window.addEventListener("resize", handleResize);
+    setWidth(window.innerWidth);
+    setHeight(window.innerHeight);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
   return (
-    <div className='h-[750px] sm:h-[700px] bg-[#223544] relative sm:flex sm:justify-center p-4'>
-      <div className='absolute top-28 flex flex-col sm:items-center gap-16 sm:gap-32'>
-        <h1 className='font-bold leading-3 text-3xl text-slate-200'>
-          Our Luxurious Fleet
-        </h1>
-        <div className='flex flex-col sm:flex-row mx-auto max-w-[1056px] gap-6 h-[270px]'>
-          <div className='flex flex-row sm:flex-col sm:p-[18px]'>
-            {/* add svg */}
-            <div className='flex flex-col sm:text-center gap-5'>
-              <p className='font-medium text-xl sm:text-center'>
-                Elevate Your Business Image
-              </p>
-              <p className='font-normal text-sm text-[#B2B2B2]'>
-                For success in business, a commanding presence is vital. Making
-                an impact starts with your appearance. Arrive at meetings
-                impeccably groomed to leave a lasting impression.
-              </p>
-            </div>
+    <div className='relative mt-16 flex flex-col w-full 2xl:min-h-[650px] xl:min-h-[560px] md:min-h-[500px] min-h-[500px] max-h-[1000px] bg-[#223544]'>
+      <div className='service-home-banner absolute sm:top-0 -top-6 left-0 right-0 h-auto sm:min-h-[328px] min-h-[328px]' />
+      <div className='sm:h-full h-full flex flex-col justify-center items-center sm:pt-[28px] pt-5'>
+        <div className='relative z-[5] sm:text-center text-left px-5'>
+          <h1 className='lg:text-[36px] mt-5 text-3xl leading-[44px] font-bold drop-shadow-[25%]'>
+            {mainTitle}
+          </h1>
+        </div>
+
+        <div className='flex gap-4 mx-auto w-[1052px] h-[270px] justify-between z-[10] mt-14'>
+          <div className='text-center w-[336px] p-[18px] flex flex-col gap-4  items-center'>
+            <img
+              className='w-[30px] h-[30px]'
+              src='/images/airports/service.png'
+              alt='services logo'
+            />
+            <h3 className='font-medium text-xl leading-[26px] text-[#FFFFFF] break-words whitespace-pre-wrap'>
+              Elevate Your Business Image
+            </h3>
+            <p className='font-normal text-sm left-5 text-[#B2B2B2]'>
+              For success in business, a commanding presence is vital. Making an
+              impact starts with your appearance. Arrive at meetings impeccably
+              groomed to leave a lasting impression.
+            </p>
           </div>
-          <div className='flex flex-col sm:p-[18px]'>
-            {/* add svg */}
-            <div className='flex flex-col sm:text-center gap-5 '>
-              <p className='font-medium text-xl sm:text-center'>
-                Comfort, Luxury, and Safety in Transportation
-              </p>
-              <p className='font-normal text-sm text-[#B2B2B2]'>
-                The convenience of having the driver navigate traffic allows for
-                a stress-free journey, allowing you to focus on the meeting at
-                hand. Hiring a business-class chauffeur service can also be
-                cost-effective for companies needing regular transportation for
-                employees or clients.
-              </p>
-            </div>
+          <div className='text-center p-[18px] w-[336px] flex flex-col gap-4 items-center'>
+            <img
+              className='w-[30px] h-[30px]'
+              src='/images/airports/service.png'
+              alt='services logo'
+            />
+            <h3 className='font-medium text-xl leading-[26px] text-[#FFFFFF]'>
+              Comfort, Luxury, and Safety in Transportation
+            </h3>
+            <p className='font-normal text-sm left-5 text-[#B2B2B2]'>
+              The comfort & luxury of a business class chauffeur service,
+              equipped with all amenities, can make the journey more pleasant &
+              relaxing. Safety and security are also paramount, with experienced
+              and vetted drivers at the helm.
+            </p>
           </div>
-          <div className='flex flex-col sm:p-[18px]'>
-            {/* add svg */}
-            <div className='flex flex-col sm:text-center gap-5 '>
-              <p className='font-medium text-xl sm:text-center'>
-                Efficiency and Cost-Effectiveness
-              </p>
-              <p className='font-normal text-sm text-[#B2B2B2]'>
-                The convenience of having the driver navigate traffic allows for
-                a stress-free journey, allowing you to focus on the meeting at
-                hand. Hiring a business-class chauffeur service can also be
-                cost-effective for companies needing regular transportation for
-                employees or clients.
-              </p>
-            </div>
+          <div className='text-center p-[18px] w-[336px] flex flex-col gap-4 items-center'>
+            <img
+              className='w-[30px] h-[30px]'
+              src='/images/airports/service.png'
+              alt='services logo'
+            />
+            <h3 className='font-medium text-xl leading-[26px] text-[#FFFFFF]'>
+              Efficiency and Cost-Effectiveness
+            </h3>
+            <p className='font-normal text-sm left-5 text-[#B2B2B2]'>
+              The convenience of having the driver navigate traffic allows for a
+              stress-free journey, allowing you to focus on the meeting at hand.
+              Hiring a business-class chauffeur service can also be
+              cost-effective for companies needing regular transportation for
+              employees or clients.
+            </p>
           </div>
+        </div>
+      </div>
+      <div className='bg-[#223544] w-[933px] p-[16px] border-[0.4px] border-gray-700 mx-auto mt-20 rounded-2xl flex justify-around shadow-md'>
+        <Link
+          href={"#"}
+          className='px-[24px] py-[4px] bg-[#FEF8F4] rounded-md text-pry-500 border border-pry-500 text-base font-medium'
+        >
+          Buisness Class
+        </Link>
+        <Link
+          href={"#"}
+          className='px-[24px] py-[4px] bg-slate-600/80 text-white hover:bg-[#FEF8F4] rounded-md hover:text-pry-500 border border-transparent hover:border-pry-500 text-base font-medium transition ease-in delay-75'
+        >
+          First Class
+        </Link>
+        <Link
+          href={"#"}
+          className='px-[24px] py-[4px] bg-slate-600/80 text-white hover:bg-[#FEF8F4] rounded-md hover:text-pry-500 border border-transparent hover:border-pry-500 text-base font-medium transition ease-in delay-75'
+        >
+          Luxury Class
+        </Link>
+        <Link
+          href={"#"}
+          className='px-[24px] py-[4px] bg-slate-600/80 text-white hover:bg-[#FEF8F4] rounded-md hover:text-pry-500 border border-transparent hover:border-pry-500 text-base font-medium transition ease-in delay-75'
+        >
+          Electric
+        </Link>
+        <Link
+          href={"#"}
+          className='px-[24px] py-[4px] bg-slate-600/80 text-white hover:bg-[#FEF8F4] rounded-md hover:text-px-[24px] py-[4px] bg-slate-600/80 text-white hover:bg-[#FEF8F4] rounded-md hover:text-pry-500 border border-transparent hover:border-pry-500 text-base font-medium transition ease-in delay-75'
+        >
+          SUV
+        </Link>
+        <Link
+          href={"#"}
+          className='px-[24px] py-[4px] bg-slate-600/80 text-white hover:bg-[#FEF8F4] rounded-md hover:text-pry-500 border border-transparent hover:border-pry-500 text-base font-medium transition ease-in delay-75'
+        >
+          MVP
+        </Link>
+        <Link
+          href={"#"}
+          className='px-[24px] py-[4px] bg-slate-600/80 text-white hover:bg-[#FEF8F4] rounded-md hover:text-pry-500 border border-transparent hover:border-pry-500 text-base font-medium transition ease-in delay-75'
+        >
+          Sprinter
+        </Link>
+      </div>
+
+      <div className='sm:w-[74px] sm:h-[74px] w-12 h-12 absolute sm:-bottom-[36px] -bottom-6 left-2/4 -translate-x-2/4 z-10 bg-[#2F4456] flex items-center justify-center flex-col p-4 border border-[#FFFFFF] border-opacity-20 rounded-lg cursor-pointer'>
+        <div className='animate-bounce'>
+          <img
+            alt='arrow-down'
+            className='w-8 h-8'
+            src='/rolnew/global/icons/arrow-down.svg'
+          />
+          <img
+            alt='arrow-down'
+            className='w-8 h-8 -mt-5'
+            src='/rolnew/global/icons/arrow-down.svg'
+          />
+          <img
+            alt='arrow-down'
+            className='w-8 h-8 -mt-5'
+            src='/rolnew/global/icons/arrow-down.svg'
+          />
         </div>
       </div>
     </div>
