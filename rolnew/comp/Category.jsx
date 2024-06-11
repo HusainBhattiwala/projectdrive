4df9498f15eed9a4238820e25bd21a25039511fd
Husain007 <husainbhattiwala5253@gmail.com>
2024-06-11 14:42:20 +0530

    fix only unique name for category

--- rolnew/comp/Category.jsx
+++ rolnew/comp/Category.jsx
@@ -5,6 +5,11 @@ import { useEffect, useState } from "react";
 function Category({ categorys = [], setCarData }) {
   const [activeCat, setActiveTab] = useState(0);
 
+  const uniqueCategories = categorys.filter(
+    (category, index, self) =>
+      index === self.findIndex((c) => c.vehCatName === category.vehCatName)
+  );
+
   useEffect(() => {
     setActiveTab(0);
   }, []);
@@ -12,7 +17,7 @@ function Category({ categorys = [], setCarData }) {
   return (
     <div className='sm:py-[44px]'>
       <div className='flex flex-nowrap gap-2 sm:gap-2 sm:bg-[#223544] md:gap-2 sm:max-w-[70%] md:w-[933px] sm:border border-gray-700 p-4 rounded-2xl overflow-x-auto mx-auto sm:justify-between justify-between items-center scroll no-scroll sm:shadow-[1px_1px_9px_-2px_rgba(0,0,0,0.75)]'>
-        {categorys.map((category, i) => (
+        {uniqueCategories.map((category, i) => (
           <button
             type='button'
             className={`flex-none border border-transparent rounded-md px-6 py-1 group ${
