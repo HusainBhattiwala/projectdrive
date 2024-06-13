110055f197651147659a43b32bc431a8559d7299
Husain007 <husainbhattiwala5253@gmail.com>
2024-06-13 09:29:07 +0530

    ui changes fleet page

--- rolnew/comp/Contact.jsx
+++ rolnew/comp/Contact.jsx
@@ -1,12 +1,15 @@
-import React from 'react';
-import Container from 'rolnew/comp/Container';
-import Title from 'rolnew/section/home/Title';
-import ContactDetails from './ContactDetails';
+import React from "react";
+import Container from "rolnew/comp/Container";
+import Title from "rolnew/section/home/Title";
+import ContactDetails from "./ContactDetails";
 
 function Contact() {
   return (
-    <Container className="bg-[#081017] sm:py-[100px] py-8 text-center">
-      <Title subTile="Let’s get in touch!" mainTitle="Contact us for quick and friendly support." />
+    <Container className='bg-[#11202D] sm:py-[100px] py-8 text-center'>
+      <Title
+        subTile='Let’s get in touch!'
+        mainTitle='Contact us for quick and friendly support.'
+      />
       <ContactDetails />
     </Container>
   );
