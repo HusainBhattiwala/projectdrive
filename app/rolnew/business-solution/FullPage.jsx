66e9942f12d9fbbc1cf570ed2daa589cc33384a0
Husain007 <husainbhattiwala5253@gmail.com>
2024-06-13 22:18:52 +0530

    buisness page integration

--- app/rolnew/business-solution/FullPage.jsx
+++ app/rolnew/business-solution/FullPage.jsx
@@ -1,14 +1,26 @@
-'use client';
+"use client";
 
-import Hero from 'rolnew/comp/Hero';
-import ServiceComp from 'rolnew/comp/ServiceComp';
-import BusinessForm from 'rolnew/section/business/BusinessForm';
+import Hero from "rolnew/comp/Hero";
+import ServiceComp from "rolnew/comp/ServiceComp";
+import BusinessForm from "rolnew/section/business/BusinessForm";
+import Contact from "rolnew/comp/Contact";
+import FAQ from "rolnew/comp/FAQ";
+import OurFeet from "rolnew/section/home/OurFeet";
 
 function FullPage() {
   return (
     <>
-      <Hero title="Tailored Business Transport Assistance" description="Experience the epitome of luxury travel with RolDrive's personal chauffeur services. From business trips to leisurely excursions, we are dedicated to providing you with unparalleled comfort, convenience, and sophistication." formContent={<BusinessForm />} mainBgUrl="/rolnew/services/business-solution-banner.svg" titleWidth="max-w-[533px]" />
+      <Hero
+        title='Tailored Business Transport Assistance'
+        description="Experience the epitome of luxury travel with RolDrive's personal chauffeur services. From business trips to leisurely excursions, we are dedicated to providing you with unparalleled comfort, convenience, and sophistication."
+        formContent={<BusinessForm />}
+        mainBgUrl='/rolnew/services/business-solution-banner.svg'
+        titleWidth='max-w-[533px]'
+      />
       <ServiceComp />
+      <OurFeet />
+      <FAQ />
+      <Contact />
     </>
   );
 }
