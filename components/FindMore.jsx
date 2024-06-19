b7e0884113b77cc56ba8a2517374ae0273897e96
MustafaAliAkbar <mustafa.a@rubikonlabs.com>
2024-06-19 19:33:06 +0530

    Lint Fix Error

--- components/FindMore.jsx
+++ components/FindMore.jsx
@@ -3,59 +3,59 @@ export default function FindMoreComponent({ findMoreData }) {
   const items = new Array(24).fill('Name');
 
   return (
-    <div className='bg-[#11202D] text-black py-12'>
-      <div className='container mx-auto px-6'>
-        <h2 className='text-center text-3xl font-bold mb-8'>Find More</h2>
-        <div className='grid grid-cols-4 gap-4 border-b pb-10'>
+    <div className="bg-[#11202D] text-black py-12">
+      <div className="container mx-auto px-6">
+        <h2 className="text-center text-3xl font-bold mb-8">Find More</h2>
+        <div className="grid grid-cols-4 gap-4 border-b pb-10">
           {items.map((item, index) => (
-            <div key={index} className='text-center'>
-              <p className='text-lg'>{item}</p>
+            <div key={index} className="text-center">
+              <p className="text-lg">{item}</p>
             </div>
           ))}
-          <div className='col-span-4'>
-            <hr className='border-t border-gray-300 my-6' />
+          <div className="col-span-4">
+            <hr className="border-t border-gray-300 my-6" />
           </div>
           {items.map((item, index) => (
-            <div key={`second-set-${index}`} className='text-center'>
-              <p className='text-lg'>{item}</p>
+            <div key={`second-set-${index}`} className="text-center">
+              <p className="text-lg">{item}</p>
             </div>
           ))}
-          <div className='col-span-4'>
-            <hr className='border-t border-gray-300 my-6' />
+          <div className="col-span-4">
+            <hr className="border-t border-gray-300 my-6" />
           </div>
           {items.map((item, index) => (
-            <div key={`second-set-${index}`} className='text-center'>
-              <p className='text-lg'>{item}</p>
+            <div key={`second-set-${index}`} className="text-center">
+              <p className="text-lg">{item}</p>
             </div>
           ))}
-          <div className='col-span-4 mx-4'>
-            <hr className='border-t border-gray-300 my-6 mx-6 px-8' />
+          <div className="col-span-4 mx-4">
+            <hr className="border-t border-gray-300 my-6 mx-6 px-8" />
           </div>
           {items.map((item, index) => (
-            <div key={`second-set-${index}`} className='text-center'>
-              <p className='text-lg'>{item}</p>
+            <div key={`second-set-${index}`} className="text-center">
+              <p className="text-lg">{item}</p>
             </div>
           ))}
-          <div className='col-span-4 mx-4'>
-            <hr className='border-t border-gray-300 my-6 mx-6 px-8' />
+          <div className="col-span-4 mx-4">
+            <hr className="border-t border-gray-300 my-6 mx-6 px-8" />
           </div>
           {items.map((item, index) => (
-            <div key={`second-set-${index}`} className='text-center'>
-              <p className='text-lg'>{item}</p>
+            <div key={`second-set-${index}`} className="text-center">
+              <p className="text-lg">{item}</p>
             </div>
           ))}
         </div>
 
-        <article className='sm:mt-20 sm:mx-10 sm:px-4 sm:text-left'>
-          <h2 className='my-10 sm:text-center sm:my-20 text-lg font-bold sm:text-3xl text-[#FFFFFF]'>
+        <article className="sm:mt-20 sm:mx-10 sm:px-4 sm:text-left">
+          <h2 className="my-10 sm:text-center sm:my-20 text-lg font-bold sm:text-3xl text-[#FFFFFF]">
             More about RolDrive’s Luxury Personal Chauffeur Services
           </h2>
           {findMoreData?.map((data, index) => (
-            <div className='space-y-2' key={index}>
-              <h4 className='mt-5 sm:mt-10 sm:text-xl text-lg font-medium text-[#B2B2B2]'>
+            <div className="space-y-2" key={index}>
+              <h4 className="mt-5 sm:mt-10 sm:text-xl text-lg font-medium text-[#B2B2B2]">
                 {data?.title}
               </h4>
-              <p className='text-sm text-[#B2B2B2]'>{data?.desc}</p>
+              <p className="text-sm text-[#B2B2B2]">{data?.desc}</p>
             </div>
           ))}
         </article>
