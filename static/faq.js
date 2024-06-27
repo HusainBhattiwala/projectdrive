94eda751af4dd2b06e24b1a4e5e84237610aed9b
Husain007 <husainbhattiwala5253@gmail.com>
2024-06-27 20:34:20 +0530

    content changes

--- static/faq.js
+++ static/faq.js
@@ -4,25 +4,21 @@ export const homePageFaq = [
     faqData: [
       {
         question: 'How can I book a chauffeur service with RolDrive?',
-        answer:
-          'Booking with RolDrive, renowned for offering the best chauffeur service, is a streamlined process designed for client convenience and satisfaction. Book RolDrive’s chauffeur service through our official website or mobile app, where you can select your desired service, date, time, and vehicle preference.',
+        ans: 'Booking with RolDrive, renowned for offering the best chauffeur service, is a streamlined process designed for client convenience and satisfaction. Book RolDrive’s chauffeur service through our official website or mobile app, where you can select your desired service, date, time, and vehicle preference.',
       },
       {
         question: 'When will I receive my chauffeur and vehicle details?',
-        answer:
-          'We assign a chauffeur/vehicle 24 hours before the booking date and time, and an email with the chauffeur and vehicle details will be sent to you.',
+        ans: 'We assign a chauffeur/vehicle 24 hours before the booking date and time, and an email with the chauffeur and vehicle details will be sent to you.',
       },
       {
         question:
           'Is advance booking required for RolDrive’s luxury chauffeur service?',
-        answer:
-          "Advance booking for RolDrive's luxury chauffeur service is highly recommended to ensure that you secure a vehicle that suits your specific needs and preferences. Early reservation allows for a more personalised service, giving RolDrive ample time to understand and prepare for any special requests you may have, whether it's a preference for a particular type of vehicle, the need for child seats, or accommodating extra luggage. It also ensures availability, particularly during peak periods or special events when demand for luxury chauffeur services spikes.",
+        ans: "Advance booking for RolDrive's luxury chauffeur service is highly recommended to ensure that you secure a vehicle that suits your specific needs and preferences. Early reservation allows for a more personalised service, giving RolDrive ample time to understand and prepare for any special requests you may have, whether it's a preference for a particular type of vehicle, the need for child seats, or accommodating extra luggage. It also ensures availability, particularly during peak periods or special events when demand for luxury chauffeur services spikes.",
       },
       {
         question:
           'What information do I need to provide when booking a ride with RolDrive?',
-        answer:
-          'When booking a chauffeur service with RolDrive, it’s important to provide comprehensive information to ensure the service is tailored to your specific needs. This includes your pickup and drop-off locations to plan the route accurately, the desired date and time of the service to schedule your journey, and the number of passengers to select an appropriately sized vehicle. Additionally, specifying any special requirements, such as child seats for young travellers or extra luggage space for those carrying more items, helps RolDrive customise the experience. Providing detailed information during booking enables RolDrive to offer a personalised, efficient, and comfortable chauffeur service, aligning with our commitment to excellence in customer satisfaction.',
+        ans: 'When booking a chauffeur service with RolDrive, it’s important to provide comprehensive information to ensure the service is tailored to your specific needs. This includes your pickup and drop-off locations to plan the route accurately, the desired date and time of the service to schedule your journey, and the number of passengers to select an appropriately sized vehicle. Additionally, specifying any special requirements, such as child seats for young travellers or extra luggage space for those carrying more items, helps RolDrive customise the experience. Providing detailed information during booking enables RolDrive to offer a personalised, efficient, and comfortable chauffeur service, aligning with our commitment to excellence in customer satisfaction.',
       },
     ],
   },
@@ -31,14 +27,12 @@ export const homePageFaq = [
     faqData: [
       {
         question: 'How do I cancel a RolDrive chauffeur service booking?',
-        answer:
-          'For any cancellations, please access your RolDrive online account via the website or app to cancel your ride.',
+        ans: 'For any cancellations, please access your RolDrive online account via the website or app to cancel your ride.',
       },
       {
         question:
           'How many hours in advance do I need to cancel RolDrive’s chauffeur service in order to receive a full refund?',
-        answer:
-          'To receive a full refund, please cancel your RolDrive chauffeur service booking at least 12 hours before booking time.',
+        ans: 'To receive a full refund, please cancel your RolDrive chauffeur service booking at least 12 hours before booking time.',
       },
     ],
   },
@@ -48,35 +42,29 @@ export const homePageFaq = [
       {
         question:
           'Are all vehicles from RolDrive’s luxury chauffeur service fleet available in all locations of service?',
-        answer:
-          "No. Not all vehicles in RolDrive's luxury chauffeur service fleet are available at every location. To ascertain the availability of specific vehicles in your desired area, initiate a booking through RolDrive's platform. During this process, enter your location details, and the system will display the vehicles available in that region. This approach ensures you can select from the options that are actually accessible for your journey, allowing for a tailored and satisfactory service experience. This method facilitates a streamlined and efficient booking process, aligning with RolDrive’s commitment to providing bespoke and luxury transportation solutions.",
+        ans: "No. Not all vehicles in RolDrive's luxury chauffeur service fleet are available at every location. To ascertain the availability of specific vehicles in your desired area, initiate a booking through RolDrive's platform. During this process, enter your location details, and the system will display the vehicles available in that region. This approach ensures you can select from the options that are actually accessible for your journey, allowing for a tailored and satisfactory service experience. This method facilitates a streamlined and efficient booking process, aligning with RolDrive’s commitment to providing bespoke and luxury transportation solutions.",
       },
       {
         question:
           'Does RolDrive’s luxury chauffeur service offer premium and rare vehicles?',
-        answer:
-          "Yes, RolDrive's luxury chauffeur service stands out for its inclusion of premium and rare vehicles, catering to clients who seek the utmost in luxury and exclusivity. With prestigious options like Rolls Royce, Maybach, and Bentley in our fleet, RolDrive ensures that clients can experience the pinnacle of automotive luxury and sophistication. These vehicles are synonymous with opulence, offering unparalleled comfort, state-of-the-art technology, and a ride experience that is both smooth and memorable. For those looking to make a grand entrance or simply indulge in the luxury of high-end transportation, RolDrive provides the perfect solution with its selection of elite vehicles.",
+        ans: "Yes, RolDrive's luxury chauffeur service stands out for its inclusion of premium and rare vehicles, catering to clients who seek the utmost in luxury and exclusivity. With prestigious options like Rolls Royce, Maybach, and Bentley in our fleet, RolDrive ensures that clients can experience the pinnacle of automotive luxury and sophistication. These vehicles are synonymous with opulence, offering unparalleled comfort, state-of-the-art technology, and a ride experience that is both smooth and memorable. For those looking to make a grand entrance or simply indulge in the luxury of high-end transportation, RolDrive provides the perfect solution with its selection of elite vehicles.",
       },
       {
         question:
           'The passenger is VVIP. What vehicle does RolDrive recommend?',
-        answer:
-          'For the complete RolDrive experience for your VVIP passenger. We recommend the Rolls Royce Phantom VIII, Mercedes Maybach or Bentley Mulsanne. These cars come with high-end amenities that will make your VVIP passenger feel like royalty.',
+        ans: 'For the complete RolDrive experience for your VVIP passenger. We recommend the Rolls Royce Phantom VIII, Mercedes Maybach or Bentley Mulsanne. These cars come with high-end amenities that will make your VVIP passenger feel like royalty.',
       },
       {
         question: 'Do you allow pets inside the vehicle?',
-        answer:
-          'Yes, we provide pet-friendly chauffeurs on demand. Please inform us when booking.',
+        ans: 'Yes, we provide pet-friendly chauffeurs on demand. Please inform us when booking.',
       },
       {
         question: 'How old will my RolDrive chauffeured vehicle be?',
-        answer:
-          'All of RolDrive’s chauffeured vehicles are less than three years old, and we generally send the most recent model in the industry at the time.',
+        ans: 'All of RolDrive’s chauffeured vehicles are less than three years old, and we generally send the most recent model in the industry at the time.',
       },
       {
         question: 'What is the luggage capacity of the vehicle I am booking?',
-        answer:
-          'To know more about our vehicle luggage capacity, please visit our fleet section on the Home page where all the details are present for every vehicle we offer.',
+        ans: 'To know more about our vehicle luggage capacity, please visit our fleet section on the Home page where all the details are present for every vehicle we offer.',
       },
     ],
   },
@@ -86,8 +74,7 @@ export const homePageFaq = [
       {
         question:
           'What are the payment options available for me to book a RolDrive chauffeur service?',
-        answer:
-          'We accept all major credit and debit cards for payments. Contact our finance department for direct bank transfer at booking@roldrive.com',
+        ans: 'We accept all major credit and debit cards for payments. Contact our finance department for direct bank transfer at booking@roldrive.com',
       },
     ],
   },
@@ -96,23 +83,19 @@ export const homePageFaq = [
     faqData: [
       {
         question: 'Are RolDrive’s chauffeurs trained at defensive driving?',
-        answer:
-          "Yes, RolDrive's chauffeurs are trained in defensive driving, ensuring they possess the skills necessary to navigate the roads safely and efficiently. This training equips them to anticipate and effectively manage any potential hazards on the road, from sudden weather changes to erratic behaviour from other drivers. Their proficiency in defensive driving not only enhances safety but also contributes to the overall smoothness and reliability of the journey, providing passengers with a sense of security and peace of mind. This meticulous attention to driver training reflects RolDrive's commitment to delivering a premium service where safety is paramount.",
+        ans: "Yes, RolDrive's chauffeurs are trained in defensive driving, ensuring they possess the skills necessary to navigate the roads safely and efficiently. This training equips them to anticipate and effectively manage any potential hazards on the road, from sudden weather changes to erratic behaviour from other drivers. Their proficiency in defensive driving not only enhances safety but also contributes to the overall smoothness and reliability of the journey, providing passengers with a sense of security and peace of mind. This meticulous attention to driver training reflects RolDrive's commitment to delivering a premium service where safety is paramount.",
       },
       {
         question: 'Will my RolDrive chauffeur help me with my luggage?',
-        answer:
-          'Yes, your RolDrive chauffeur will gladly offer assistance with your luggage.',
+        ans: 'Yes, your RolDrive chauffeur will gladly offer assistance with your luggage.',
       },
       {
         question: 'Does a RolDrive chauffeur wear a suit and tie?',
-        answer:
-          'Yes, always. RolDrive chauffeurs must adhere to a strict dress code of wearing a suit and tie.',
+        ans: 'Yes, always. RolDrive chauffeurs must adhere to a strict dress code of wearing a suit and tie.',
       },
       {
         question: 'Will I be notified when my RolDrive chauffeur arrives?',
-        answer:
-          'Yes, as soon as your RolDrive chauffeur arrives, an email will be sent to your registered email address. Your chauffeur will also send you a text message with his/her exact location, which is usually just outside the pick-up address.',
+        ans: 'Yes, as soon as your RolDrive chauffeur arrives, an email will be sent to your registered email address. Your chauffeur will also send you a text message with his/her exact location, which is usually just outside the pick-up address.',
       },
     ],
   },
@@ -122,13 +105,11 @@ export const homePageFaq = [
       {
         question:
           "I'm flying from the United Kingdom. How many hours should I arrive at the airport before my flight?",
-        answer:
-          'Most airlines recommend arriving three hours before an international flight and two hours before a domestic flight. If you checked in online, the latest arrival times for international flights are 2 hours and 1.5 hours prior for domestic flights.',
+        ans: 'Most airlines recommend arriving three hours before an international flight and two hours before a domestic flight. If you checked in online, the latest arrival times for international flights are 2 hours and 1.5 hours prior for domestic flights.',
       },
       {
         question: 'Where will I meet my chauffeur?',
-        answer:
-          "Your RolDrive chauffeur will be waiting at arrivals with your name on a paging board as soon as the flight lands. Your chauffeur will also send you an SMS message with the exact meeting location information. You will also receive these details via email, along with your chauffeur's exact GPS location within the terminal.",
+        ans: "Your RolDrive chauffeur will be waiting at arrivals with your name on a paging board as soon as the flight lands. Your chauffeur will also send you an SMS message with the exact meeting location information. You will also receive these details via email, along with your chauffeur's exact GPS location within the terminal.",
       },
       {
         question: 'My flight is delayed, will my chauffeur wait for me?',
@@ -137,19 +118,16 @@ export const homePageFaq = [
       },
       {
         question: 'Will I be charged for tolls and parking by RolDrive?',
-        answer:
-          'No, all charges are included in the booking price. We have a no-hidden-costs policy at RolDrive.',
+        ans: 'No, all charges are included in the booking price. We have a no-hidden-costs policy at RolDrive.',
       },
       {
         question: 'What happens if my flight details change?',
-        answer:
-          'Please call or email us and we will update your reservation with the new flight information.',
+        ans: 'Please call or email us and we will update your reservation with the new flight information.',
       },
       {
         question:
           'My flight will be arriving early. Will my RolDrive chauffeur be there for me?',
-        answer:
-          'Yes, your RolDrive chauffeur will monitor your flight and make certain that your chauffeur is in the arrivals hall before you leave customs.',
+        ans: 'Yes, your RolDrive chauffeur will monitor your flight and make certain that your chauffeur is in the arrivals hall before you leave customs.',
       },
     ],
   },
