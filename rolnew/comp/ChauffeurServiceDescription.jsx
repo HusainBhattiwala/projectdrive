f36b75dfdf11607ca7a1d085259812e1a56bd4b4
MustafaAliAkbar <mustafa.a@rubikonlabs.com>
2024-06-29 12:08:07 +0530

    font size

--- rolnew/comp/ChauffeurServiceDescription.jsx
+++ rolnew/comp/ChauffeurServiceDescription.jsx
@@ -4,13 +4,13 @@ function ChauffeurServiceDescription({ ChauffeurServiceDescription }) {
   console.log("services:", ChauffeurServiceDescription);
   return (
     <div className='bg-[#223544]'>
-      <div className='sm:pt-16 sm:pb-11 px-[20px] lg:px-0 py-[24px]'>
+      <div className='sm:pt-16 sm:pb-11 px-[10px] lg:px-0 py-[24px]'>
         {ChauffeurServiceDescription?.map((p, i) => (
           <div
             key={i}
-            className='font-normal sm:font-medium text-[10px] leading-[14px] sm:leading-0 sm:text-sm text-[#B2B2B2] max-w-[1112px] mx-auto sm:mb-5'
+            className='font-normal sm:font-medium text-[15px] leading-[14px] sm:leading-0 sm:text-sm text-[#B2B2B2] max-w-[1212px] mx-auto sm:mb-5'
           >
-            <div className='text-[#B2B2B2] w-[94%]'>{p.p}</div>
+            <div className='text-[#B2B2B2] text-[16px] w-[100%]'>{p.p}</div>
           </div>
         ))}
       </div>
