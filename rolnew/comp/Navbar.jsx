2e58b4640f99ebf40e3144c5d601a5b1f5f205b8
HusainBhattiwala <husainbhattiwala5253@gmail.com>
2024-07-01 21:31:29 +0530

    login page styles changes

--- rolnew/comp/Navbar.jsx
+++ rolnew/comp/Navbar.jsx
@@ -267,7 +267,7 @@ export default function Navbar() {
                   <p className="font-medium">Contact Us</p>
                 </button>
               </a>
-              <Link href="/login">
+              <Link href="/rolnew/login">
                 <Button className="w-[120px]" white>
                   Sign In
                 </Button>
