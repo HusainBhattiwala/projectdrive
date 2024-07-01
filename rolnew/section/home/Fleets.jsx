036c5d51f25c27264f6cb0ad9097fccc32a97b1a
HusainBhattiwala <husainbhattiwala5253@gmail.com>
2024-07-01 19:17:11 +0530

    styling fix

--- rolnew/section/home/Fleets.jsx
+++ rolnew/section/home/Fleets.jsx
@@ -50,22 +50,38 @@ export default function Fleets({ slideData = [], showBooking = false }) {
       >
         {slidesToShow.map((item, index) => (
           <SwiperSlide key={`${item.id}-${index}`}>
-            <div className='max-w-[574px]'>
-              <img src={item?.img} alt={item?.name} />
-            </div>
-            <p className='text-center text-[#E5EAFA] text-xl font-medium flex-none mb-2'>
-              {item.name}
-            </p>
+            {({ isActive }) => (
+              <div>
+                <div
+                  className={`max-w-[574px] mt-5 ${
+                    isActive
+                      ? "h-[250px] text-[#ffffff] font-bold text-2xl"
+                      : "h-[350px] text-[#E5EAFA] font-medium text-sm"
+                  }`}
+                >
+                  <img
+                    className={`object-fill ${
+                      isActive ? "h-full w-full" : "w-full"
+                    }`}
+                    src={item?.img}
+                    alt={item?.name}
+                  />
+                </div>
+                <p className='text-center text-[#E5EAFA] text-xl font-medium flex-none mb-2'>
+                  {item.name}
+                </p>
+              </div>
+            )}
           </SwiperSlide>
         ))}
       </Swiper>
-      <div className={`sm:py-[42px] py-5 ${showBooking && "mt-6"}`}>
+      <div className={`${showBooking && "pb-20"} z-50`}>
         {showBooking && (
           <Button className='flex-1 !px-14'>
             <Link href='/rolnew'>Book Now</Link>
           </Button>
         )}
-        <div className='flex justify-center items-center gap-x-3 sm:mt-10 relative'>
+        <div className='flex justify-center items-center gap-x-3 sm:mt-5 relative'>
           <div className='swiper-btn-prev pop w-12 h-12 bg-[#11202D] border border-[#223544] rounded-full flex items-center justify-center cursor-pointer'>
             <img
               alt='go-prev'
