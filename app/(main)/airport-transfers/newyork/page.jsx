7bfae60b89e94bc62edfa89e56d4b251125936c4
HusainBhattiwala <husainbhattiwala5253@gmail.com>
2024-07-05 18:14:56 +0530

    airports image updated

--- app/(main)/airport-transfers/newyork/page.jsx
+++ app/(main)/airport-transfers/newyork/page.jsx
@@ -14,6 +14,7 @@ const pageData = {
     bannerImage: '/rolnew/airports/airport-transfer-newyork.jpg',
   },
   aboutData: {
+    img: '/rolnew/airports/about/About Newyork.jpg',
     title: 'Airport Transfer New York',
     description:
       'Offering premium airport transfer New York services, we ensure a seamless connection to and from all of New York’s airports. With a fleet of luxury vehicles to take care of all your transportation needs, our professional chauffeur service provides reliable, comfortable and stylish transportation tailored to meet the needs of both business and leisure travellers. ',
@@ -25,25 +26,25 @@ const pageData = {
     desc: 'Indulge in an unparalleled chauffeur experience with our tailored services designed to meet your unique luxury airport transfer requirements. Whether you need a seamless airport transfer for your corporate travel or special event transfer, our chauffeur services are crafted to upgrade your journey. As among the best airport transfer New York service providers, embark on a journey where every detail is tailored to perfection.',
     cardData: [
       {
-        img: '/rolnew/global/card/card-image1.jpg',
+        img: '/rolnew/global/card/newyork_airports/John F. Kennedy International Airport.jpg',
         title: 'John F. Kennedy International Airport',
         cardDesc:
           'John F. Kennedy International Airport is a major international airport serving New York City and the New York metropolitan area, in the United States of America. The airport is the busiest of the seven airports in the New York airport system, the 6th-busiest airport in the United States.',
       },
       {
-        img: '/rolnew/global/card/card-image4.jpg',
+        img: '/rolnew/global/card/newyork_airports/Newark Liberty International Airport.jpg',
         title: 'Newark Liberty International Airport',
         cardDesc:
           'Newark Liberty International Airport is an international airport straddling the boundary between the cities of Newark in Essex County and Elizabeth in Union County, New Jersey, in the United States of America.',
       },
       {
-        img: '/rolnew/global/card/card-image5.jpg',
+        img: '/rolnew/global/card/newyork_airports/LaGuardia Airport.jpg',
         title: 'LaGuardia Airport',
         cardDesc:
           'LaGuardia Airport is a civil airport in East Elmhurst, Queens, New York City. The facility was established in 1929 and began operating as a public airport in 1939. It is named after former New York City mayor Fiorello La Guardia.',
       },
       {
-        img: '/rolnew/global/card/card-image6.jpg',
+        img: '/rolnew/global/card/newyork_airports/Buffalo Niagara International Airport.jpg',
         title: 'Buffalo Niagara International Airport',
         cardDesc:
           'Buffalo Niagara International Airport is in Cheektowaga, New York, United States of America. The airport serves Buffalo, New York and Niagara Falls, New York United States, and the southern Golden Horseshoe region of Ontario, Canada. It is the third-busiest airport in the state of New York and the busiest inside of the Buffalo-Niagara Falls metropolitan area.',
